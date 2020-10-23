3b32dfee19a1ef26f3753f462f193860d56a6ce7
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2020-10-23 13:16:55 -0500

    document index module

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @module hermes
+ * @description An event-processing system.
+ */
+
+/**
+ * application entrypoint
+ * @public
+ */
+async function main() {
+  console.log("Hello World 🌎");
+}
+
+export default main;
