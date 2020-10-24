4c3fc005f5ab699aadd68fc3af26f8b599f1c71a
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2020-10-24 09:04:25 -0500

    chore(jest.config.js): set code coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,17 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: [
+        '**/*.{ts}',
+        '!**/node_modules/**',
+        '!**/coverage/**',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 }
